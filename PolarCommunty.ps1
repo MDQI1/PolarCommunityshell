bbb14ffc730700cc2573e176797ff55ec20715fb
Polar Community <223210820+MDQI1@users.noreply.github.com>
2026-01-30 15:09:38 +0300

    Update plugin link to version 1.8.4

--- PolarCommunty.ps1
+++ PolarCommunty.ps1
@@ -37,7 +37,7 @@ Clear-Host
 
 # Configuration
 $pluginName = "PolarTools"
-$pluginLink = "https://github.com/MDQI1/PolarTools/releases/download/1.5.6/PolarTools_v1.5.6.zip"
+$pluginLink = "https://github.com/MDQI1/PolarTools/releases/download/v1.8.4/PolarTools_v1.8.4.zip"
 $oldPluginNames = @("luatools", "manilua", "stelenium", "PolarTools")
 
 # Hide progress bar for faster downloads
